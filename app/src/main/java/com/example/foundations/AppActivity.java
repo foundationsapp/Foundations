af3f88f6c98a382a9debd21e4198988c059b2be1
natni <44041437+natni@users.noreply.github.com>
2020-10-20 14:30:00 -0700

    Created a navigation bar for main page of app

--- app/src/main/java/com/example/foundations/AppActivity.java
+++ app/src/main/java/com/example/foundations/AppActivity.java
@@ -3,12 +3,20 @@ package com.example.foundations;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class AppActivity extends AppCompatActivity {
 
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+
     private final static String TAG = AppActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +25,18 @@ public class AppActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Profile currentProfile = intent.getParcelableExtra("userProfile");
 
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        toggle = new ActionBarDrawerToggle( this,drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
