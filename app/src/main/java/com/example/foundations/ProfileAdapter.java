36aa33c9c6ff96908f13eaf0a4a29d4ba95e80d7
jin woo choi <jinudean@gmail.com>
2020-10-26 01:23:35 -0700

    profile select show up through toast now

--- app/src/main/java/com/example/foundations/ProfileAdapter.java
+++ app/src/main/java/com/example/foundations/ProfileAdapter.java
@@ -1,21 +1,26 @@
 package com.example.foundations;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import static android.content.ContentValues.TAG;
+
 public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder> {
 
     private List<Profile> profiles;
     private final LayoutInflater inflater;
     private SetProfileHandler profileHandler;
+    private Context mContext;
 
     static class ProfileViewHolder extends RecyclerView.ViewHolder {
 
@@ -27,9 +32,10 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileV
         }
     }
 
-    ProfileAdapter(Context context, SetProfileHandler profileHandler) {
+    public ProfileAdapter(Context context, SetProfileHandler profileHandler) {
         inflater = LayoutInflater.from(context);
         this.profileHandler = profileHandler;
+        mContext = context;
     }
 
     @NonNull
@@ -45,8 +51,13 @@ public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileV
         if (profiles != null) {
             Profile current = profiles.get(position);
             holder.profileItemView.setText(current.getFullName());
-            holder.profileItemView.setOnClickListener(view -> {
-                this.profileHandler.setCurrentProfile(current);
+            holder.profileItemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "onClick: clicked on: " + current.getFullName());
+                    Toast.makeText(mContext, "You Select "+current.getFullName(), Toast.LENGTH_LONG).show();
+
+                }
             });
         }
     }
