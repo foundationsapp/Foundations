e1621b8f735b3e55e923dad59bcf5b36e023c179
jin woo choi <jinudean@gmail.com>
2020-11-01 22:34:22 -0800

    add profile picture button and go into camera

--- app/src/main/java/com/example/foundations/SignUp.java
+++ app/src/main/java/com/example/foundations/SignUp.java
@@ -1,16 +1,30 @@
 package com.example.foundations;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelStoreOwner;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Parcelable;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import java.io.File;
 
 import static android.util.Patterns.EMAIL_ADDRESS;
 
@@ -25,6 +39,15 @@ public class SignUp extends AppCompatActivity {
     Button signUpButton;
     MainViewModel mainViewModel;
 
+    String [] permission_list = new String[]{
+            Manifest.permission.CAMERA,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
+
+    String dir_path;
+    Uri contentUri;
+    ImageView profileimage;
 
 
     @Override
@@ -33,6 +56,14 @@ public class SignUp extends AppCompatActivity {
         setContentView(R.layout.signup_activity);
 
         mainViewModel = new ViewModelProvider((ViewModelStoreOwner) this).get(MainViewModel.class);
+
+        profileimage = (ImageView)findViewById(R.id.signupimage);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            requestPermissions(permission_list,0);
+        } else {
+            init();
+        }
 
         editFirstName = findViewById(R.id.edit_first_name);
         editLastName = findViewById(R.id.edit_last_name);
@@ -61,6 +92,50 @@ public class SignUp extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        for(int a1 : grantResults){
+            if(a1 == PackageManager.PERMISSION_DENIED){
+                return;
+            }
+        }
+        init();
+    }
+
+    public void init(){
+        String temp_path = getExternalFilesDir(null).getAbsolutePath();
+        dir_path = temp_path + "/Android/data/" + getPackageName();
+        File file = new File(dir_path);
+        if(file.exists() == false){
+            file.mkdir();
+        }
+    }
+    public void startCamera(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String file_name = "/tmp_" + System.currentTimeMillis() +".jpg";
+        String pic_path = dir_path + file_name;
+
+        File file = new File(pic_path);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            contentUri = FileProvider.getUriForFile(this, "com.example.Foundations.file_provider",file);
+        } else {
+            contentUri = Uri.fromFile(file);
+        }
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+        startActivityForResult(intent,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Bitmap bitmap = BitmapFactory.decodeFile(contentUri.getPath());
+            profileimage.setImageBitmap(bitmap);
+        }
     }
 
     public boolean check() {
