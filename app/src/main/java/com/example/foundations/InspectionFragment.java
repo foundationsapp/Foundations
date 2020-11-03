b702bed2637efaadf70836a76f10f9b2143449c0
kevin <kevinpettinger@gmail.com>
2020-11-03 13:39:09 -0800

    filled out inspection frag

--- app/src/main/java/com/example/foundations/InspectionFragment.java
+++ app/src/main/java/com/example/foundations/InspectionFragment.java
@@ -3,6 +3,10 @@ package com.example.foundations;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,9 +20,14 @@ public class InspectionFragment extends Fragment {
         // Required empty public constructor
     }
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inspection, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_inspection, container, false);
+        RecyclerView reportRecyclerView = view.findViewById(R.id.all_inspections_recyclerview);
+        final ReportAdapter reportAdapter = new ReportAdapter(reportRecyclerView.getContext());
+        reportRecyclerView.setAdapter(reportAdapter);
+        reportRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MainViewModel mainViewModel = new ViewModelProvider((ViewModelStoreOwner) this).get(MainViewModel.class);
+        mainViewModel.getAllReports().observe(getViewLifecycleOwner(), reportAdapter::setReports);
+        return view;
     }
 }
