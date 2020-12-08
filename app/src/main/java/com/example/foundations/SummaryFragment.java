12a1c9b68fa922889052e5a972830548f9122017
kevin <kevinpettinger@gmail.com>
2020-12-07 16:50:58 -0800

    started title page

--- app/src/main/java/com/example/foundations/SummaryFragment.java
+++ app/src/main/java/com/example/foundations/SummaryFragment.java
@@ -5,7 +5,6 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +15,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
@@ -25,27 +25,26 @@ import com.itextpdf.text.BaseColor;
 import com.itextpdf.text.Chapter;
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.Paragraph;
-import com.itextpdf.text.Phrase;
 import com.itextpdf.text.Section;
-import com.itextpdf.text.pdf.PdfPCell;
-import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
+import static com.itextpdf.text.Element.ALIGN_CENTER;
 import static com.itextpdf.text.Section.NUMBERSTYLE_DOTTED_WITHOUT_FINAL_DOT;
 
 public class SummaryFragment extends Fragment {
@@ -93,6 +92,7 @@ public class SummaryFragment extends Fragment {
         File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
         String file_path = storageDir.getAbsolutePath() + file_name;
         Document document = new Document();
+        addMetaData(document);
         PdfWriter.getInstance(document, new FileOutputStream(file_path));
         document.open();
 
@@ -186,7 +186,7 @@ public class SummaryFragment extends Fragment {
             } else{
                     try {
                         createPDF();
-                    } catch (DocumentException | IOException e) {
+                    } catch (IOException | DocumentException e) {
                         e.printStackTrace();
                     }
                 }
@@ -198,112 +198,52 @@ public class SummaryFragment extends Fragment {
 
     }
 
-//    private void savepdf(){
-//        // Document document = new Document();
-//        String file = "Foundations Report " + new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(System.currentTimeMillis());
-//        String filePath = Environment.getExternalStorageDirectory()+"/"+file+".pdf";
-//        Font smallBold = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD);
-//        try{
-//
-//            Document document = new Document();
-//            PdfWriter.getInstance(document, new FileOutputStream(filePath));
-//            document.open();
-//            addMetaData(document);
-//            addTitlePage(document);
-//            addContent(document);
-//            document.close();
-//            Toast.makeText(getActivity(), ""+ file + ".pdf" + "is saved to " + filePath , Toast.LENGTH_SHORT).show();
-//
-//        }
-//        catch(Exception e){
-//            e.printStackTrace();
-//        }
-//
-//    }
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void addTitlePage(Document document) throws IOException, DocumentException {
+        Image logo = Image.getInstance("drawable/logo.png");
+        logo.scaleToFit(400, 200);
+        logo.setAlignment(ALIGN_CENTER);
+        logo.setSpacingAfter(5);
+        logo.setSpacingBefore(5);
+        document.add(logo);
+        Paragraph title = new Paragraph(getString(R.string.title_page_title));
+        title.setAlignment(ALIGN_CENTER);
+        title.setSpacingAfter(10);
+        document.add(title);
+        Paragraph preparedFor = new Paragraph(getString(R.string.prepedfor));
+        preparedFor.setAlignment(ALIGN_CENTER);
+        document.add(preparedFor);
+        Paragraph buyer = new Paragraph(currentReport.getBuyerFirstName() + " " + currentReport.getBuyerLastName());
+        buyer.setAlignment(ALIGN_CENTER);
+        buyer.setSpacingAfter(5);
+        document.add(buyer);
+        Image propertyPhoto = Image.getInstance(currentReport.getPropertyPhoto());
+        propertyPhoto.scaleToFit(400, 200);
+        propertyPhoto.setAlignment(ALIGN_CENTER);
+        propertyPhoto.setSpacingAfter(5);
+        document.add(propertyPhoto);
+        Paragraph propertyInspected = new Paragraph("PROPERTY INSPECTED:");
+        propertyInspected.setAlignment(ALIGN_CENTER);
+        document.add(propertyInspected);
+        Paragraph address = new Paragraph(currentReport.getStreet() + "\n" + currentReport.getCity() + ", " + currentReport.getState() + " " + currentReport.getZip(),
+                FontFactory.getFont(FontFactory.HELVETICA_BOLD, 20));
+        address.setAlignment(ALIGN_CENTER);
+        document.add(address);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        LocalDateTime now = LocalDateTime.now();
+        Paragraph inspectionDate = new Paragraph("Date of Inspection: " + dtf.format(now));
+        inspectionDate.setAlignment(ALIGN_CENTER);
+        document.add(inspectionDate);
+        document.newPage();
 
+    }
 
-//    private static void addMetaData(Document document) {
-//        document.addTitle("Home Inspection By Foundations");
-//        document.addSubject("Using iText");
-//        document.addKeywords("Java, PDF, iText");
-//        document.addAuthor("Foundations");
-//        document.addCreator("Foundations");
-//    }
-//
-//
-//    private static void addTitlePage(Document document)
-//            throws DocumentException {
-//        Paragraph preface = new Paragraph();
-//        // We add one empty line
-//        addEmptyLine(preface, 1);
-//        // Lets write a big header
-//        preface.add(new Paragraph("Title of the document", catFont));
-//
-//        addEmptyLine(preface, 1);
-//        // Will create: Report generated by: _name, _date
-//        preface.add(new Paragraph(
-//                "Report generated by: " + System.getProperty("user.name") + ", " + new Date(), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-//                smallBold));
-//        addEmptyLine(preface, 3);
-//        preface.add(new Paragraph(
-//                "This document describes something which is very important ",
-//                smallBold));
-//
-//        addEmptyLine(preface, 8);
-//
-//        preface.add(new Paragraph(
-//                "This document is a preliminary version and not subject to your license agreement or any other agreement with vogella.com ;-).",
-//                redFont));
-//
-//        document.add(preface);
-//        // Start a new page
-//        document.newPage();
-//    }
-//
-//    private static void addContent(Document document) throws DocumentException {
-//        Anchor anchor = new Anchor("Basic Information", catFont);
-//        anchor.setName("First Chapter");
-//
-//        // Second parameter is the number of the chapter
-//        Chapter catPart = new Chapter(new Paragraph(anchor), 1);
-//
-//        Paragraph subPara = new Paragraph("Buyer Information", subFont);
-//        Section subCatPart = catPart.addSection(subPara);
-//        subCatPart.add(new Paragraph("First Name"));
-//        subCatPart.add(new Paragraph("Last Name"));
-//
-//        subPara = new Paragraph("Seller Information", subFont);
-//        subCatPart = catPart.addSection(subPara);
-//        subCatPart.add(new Paragraph("First Name"));
-//        subCatPart.add(new Paragraph("Last Name"));
-//
-//        // add a list
-//        createList(subCatPart);
-//        Paragraph paragraph = new Paragraph();
-//        addEmptyLine(paragraph, 5);
-//        subCatPart.add(paragraph);
-//
-//        // add a table
-//        createTable(subCatPart);
-//
-//        // now add all this to the document
-//        document.add(catPart);
-//
-//        // Next section
-//        anchor = new Anchor("Exterior", catFont);
-//        anchor.setName("Exterior");
-//
-//        // Second parameter is the number of the chapter
-//        catPart = new Chapter(new Paragraph(anchor), 1);
-//
-//        subPara = new Paragraph("Subcategory", subFont);
-//        subCatPart = catPart.addSection(subPara);
-//        subCatPart.add(new Paragraph("Doors"));
-//
-//        // now add all this to the document
-//        document.add(catPart);
-//
-//    }
+    private static void addMetaData(Document document) {
+        document.addTitle("Home Inspection By Foundations");
+        document.addKeywords("Inspection, Report, PDF, Foundations, iText");
+        document.addAuthor("Foundations");
+        document.addCreator("Foundations");
+    }
 
     private Chapter createNewCategory(String categoryName, int sectionNumber) {
         Anchor anchor = new Anchor(categoryName, catFont);
@@ -334,52 +274,4 @@ public class SummaryFragment extends Fragment {
         }
         section.setNumberStyle(NUMBERSTYLE_DOTTED_WITHOUT_FINAL_DOT);
     }
-
-//    private static void createTable(Section subCatPart)
-//            throws BadElementException {
-//        PdfPTable table = new PdfPTable(3);
-//
-//        // t.setBorderColor(BaseColor.GRAY);
-//        // t.setPadding(4);
-//        // t.setSpacing(4);
-//        // t.setBorderWidth(1);
-//
-//        PdfPCell c1 = new PdfPCell(new Phrase("Table Header 1"));
-//        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-//        table.addCell(c1);
-//
-//        c1 = new PdfPCell(new Phrase("Table Header 2"));
-//        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-//        table.addCell(c1);
-//
-//        c1 = new PdfPCell(new Phrase("Table Header 3"));
-//        c1.setHorizontalAlignment(Element.ALIGN_CENTER);
-//        table.addCell(c1);
-//        table.setHeaderRows(1);
-//
-//        table.addCell("1.0");
-//        table.addCell("1.1");
-//        table.addCell("1.2");
-//        table.addCell("2.1");
-//        table.addCell("2.2");
-//        table.addCell("2.3");
-//
-//        subCatPart.add(table);
-//
-//    }
-//
-//    private static void createList(Section subCatPart) {
-//        com.itextpdf.text.List list = new com.itextpdf.text.List(true, false, 10);
-//        list.add(new com.itextpdf.text.ListItem("First point"));
-//        list.add(new com.itextpdf.text.ListItem("Second point"));
-//        list.add(new com.itextpdf.text.ListItem("Third point"));
-//        subCatPart.add(list);
-//    }
-//
-//    private static void addEmptyLine(Paragraph paragraph, int number) {
-//        for (int i = 0; i < number; i++) {
-//            paragraph.add(new Paragraph(" "));
-//        }
-//    }
-
 }
